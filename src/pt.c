54ee0768a99cca7d0cb9b70dc937e4b3450f7957
brianej <brianemmanuel149@gmail.com>
2023-11-25 14:31:31 +0000

    Update pt.c

--- src/pt.c
+++ src/pt.c
@@ -14,6 +14,7 @@ uint32_t page_table_faults;
 uint32_t page_table_faults_with_dirty_page;
 
 uint32_t empty = 0;
+int last_used = 0;
 
 // Gets the tag of the address
 uint32_t vpn_getter(uint32_t pa){
@@ -26,25 +27,35 @@ uint32_t vpn_getter(uint32_t pa){
 	return vpn;
 }
 
-// LRU
- void LRU(block_t* Set[],block_t entry)
- {
- 	// assign last value the new block
- 	Set[set_size-1].tag = entry.tag;
- 	Set[set_size-1].valid = entry.valid;
-	Set[set_size-1].dirty = entry.dirty;
- }
-
  // recently used 
- void recently_used(block_t* Set[],int index)
+ void recently_used(uint32_t PPN)
  {
- 	block_t temp = Set[index];
+    int index = 0;
 
+    // find where the index of the ppn in the used page list
+    for (int i = 0; i < 256; i++){
+        if (used_page_list[i].ppn == PPN){
+            index = i;
+            break;
+        }
+    }
+
+ 	page_t temp = used_page_list[index];
+
+    // move down the page_t to make space for the recently used ppn
  	for(int i = index; i > 0;i--){
- 		Set[i]= Set[i-1];
+ 		used_page_list[i]= used_page_list[i-1];
  	}
 
- 	Set[0] = temp;
+ 	used_page_list[0] = temp;
+ }
+
+ // add ppn into used page list
+ void add_to_used(uint32_t PPN, uint32_t index){
+    pt_entry_t *page_table_entry = &page_table[index];
+    used_page_list[last_used].ppn = PPN;
+    used_page_list[last_used].page_table_entry = page_table_entry;
+    last_used++;
  }
 
 void initialize_pt_system()
@@ -73,6 +84,10 @@ void init_pt() {
 		page_table[i].dirty = 0;
 		page_table[i].PPN = 0;
 	}
+
+    page_table_total_accesses = 0;
+    page_table_faults = 0;
+    page_table_faults_with_dirty_page = 0;
 }
 
 void init_free_page_list(page_t** free_page_list){
@@ -91,39 +106,62 @@ int check_page_table(uint32_t address){
     //return PPN if the page is hit
     uint32_t vpn = vpn_getter(address);
 
+    page_table_total_accesses++;
+
     // If the valid bit is there
     if (page_table[vpn].present == 1){
+        recently_used(page_table[vpn].PPN);
         return page_table[vpn].PPN;
     }else {
-        // if theres no more free page list to use, use lru on the used page list
-        if (empty){
-
-        }
-
-        page_t *free_ppn = get_free_page();
-        uint32_t address_ppn =  free_ppn->ppn;
-        page_table[vpn].PPN = address_ppn;
-        page_table[vpn].present = 1;
+        page_table_faults++;
+       return -1;
     }
-
-    return vpn;
 }
 
 // Extract the VPN from the address and use it.
 void update_page_table(uint32_t address, uint32_t PPN){
     //set PPN for VPN in page table entry
     //set present bit in page table entry
+    uint32_t vpn = vpn_getter(address);
+
+    // clear out any entry where the vicitm page was used in the page table
+    for (int i = 0; i < pow(2,14); i++){
+        if (page_table[i].PPN == PPN){
+            page_table[i].PPN = 0;
+            page_table[i].present = 0;
+            break;
+        }
+    }
+
+    // creating new page in page table
+    page_table[vpn].PPN = PPN;
+    page_table[vpn].present = 1;
+
+    // if the used page is full, change the page table entry of the page 
+    if (last_used == 256){
+        pt_entry_t *page_table_entry = &page_table[vpn];
+        used_page_list[255].ppn = PPN;
+        used_page_list[255].page_table_entry = page_table_entry;
+    }else {
+        add_to_used(PPN,vpn);
+    }
+
+    recently_used(PPN);
     insert_or_update_tlb_entry(address, PPN);
 }
 
 //set the dirty bit of the entry to 1
 void set_dirty_bit_in_page_table(uint32_t address){
-    
+    page_table[vpn_getter(address)].dirty = 1;
 }
 
 // LRU is to be use to find the victim page
 page_t *get_victim_page(){
-    return page_table[255];
+    if (page_table[255].dirty == 1){
+        page_table_faults_with_dirty_page++;
+    }
+    // victim page is always at the bottom using our lru
+    return &page_table[255];
 }
 
 // pops a page from the free page linked-list
@@ -140,6 +178,11 @@ page_t *get_free_page(){
 // print pt entries as per the spec
 void print_pt_entries(){
     printf("\nPage Table Entries (Present-Bit Dirty-Bit PPN)\n");
+    for (int i = 0; i < ppow(2,14); i++){
+        if (page_table[i].present == 1){
+            printf("%d %d 0x%08x\n");
+        }
+    }
 }
 
 // print pt statistics as per the spec
